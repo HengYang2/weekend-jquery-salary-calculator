43bd3ffe092420954d70a66e35d47a899c7357fa
Heng Yang <hengya03@gmail.com>
2023-04-01 15:00:41 -0500

    Added an if-else condition that will prevent rows from being added if all the input fields aren't filled out. Also created logic that will erase input values once the submit button is pressed and all the fields are filled out.

--- script.js
+++ script.js
@@ -13,20 +13,34 @@ function onReady() {
 function submitHandler(event) {
     //Stop default functionalities such as refreshing the page when the submit button is pressed.
     event.preventDefault();
+
     //Create variables to hold input values:
     let firstNameInput = $('#firstNameInput').val();
     let lastNameInput = $('#lastNameInput').val();
     let idInput = $('#idInput').val();
     let titleInput = $('#titleInput').val();
     let anualSalaryInput = $('#anualSalaryInput').val();
-    //Create a tr element filled out with the input values and place it into the table:
-    $('tbody').append(`<tr>
-        <th>${firstNameInput}</th>
-        <th>${lastNameInput}</th>
-        <th>${idInput}</th>
-        <th>${titleInput}</th>
-        <th>${anualSalaryInput}</th>  
-        <th><button id="deleteButton"> Delete </th>
-    </tr>`);
+
+    //If all input boxes aren't given a value, a new row wont be added:
+    if (firstNameInput != '' && lastNameInput != '' && idInput != '' && titleInput != '' && anualSalaryInput != '') {
+        //Erase input values:
+        $('#firstNameInput').val('');
+        $('#lastNameInput').val('');
+        $('#idInput').val('');
+        $('#titleInput').val('');
+        $('#anualSalaryInput').val('');
+
+        //Create a tr element filled out with the input values and place it into the table:
+        $('tbody').append(`<tr>
+            <th>${firstNameInput}</th>
+            <th>${lastNameInput}</th>
+            <th>${idInput}</th>
+            <th>${titleInput}</th>
+            <th>${anualSalaryInput}</th>  
+            <th><button id="deleteButton"> Delete </th>
+        </tr>`);
+    } else {
+        console.log('Required fields missing!');
+    }
 }
 
