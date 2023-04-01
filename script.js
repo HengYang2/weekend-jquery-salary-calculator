66b2e17c20632009af15f376f51a682b482af54f
Heng Yang <hengya03@gmail.com>
2023-04-01 14:22:09 -0500

    Created a click listener for submitButton. Also created a function called submitHandler() that takes the input values, stores them as variables, and uses those variables to create a new row of information into the table.

--- script.js
+++ script.js
@@ -2,5 +2,31 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log("jquery working");
+    //Create an event listener for submit button
+    $('#submitButton').on('click', submitHandler);
 
 }
+
+
+
+//Callback Functions:
+function submitHandler(event) {
+    //Stop default functionalities such as refreshing the page when the submit button is pressed.
+    event.preventDefault();
+    //Create variables to hold input values:
+    let firstNameInput = $('#firstNameInput').val();
+    let lastNameInput = $('#lastNameInput').val();
+    let idInput = $('#idInput').val();
+    let titleInput = $('#titleInput').val();
+    let anualSalaryInput = $('#anualSalaryInput').val();
+    //Create a tr element filled out with the input values and place it into the table:
+    $('tbody').append(`<tr>
+        <th>${firstNameInput}</th>
+        <th>${lastNameInput}</th>
+        <th>${idInput}</th>
+        <th>${titleInput}</th>
+        <th>${anualSalaryInput}</th>  
+        <th><button id="deleteButton"> Delete </th>
+    </tr>`);
+}
+
